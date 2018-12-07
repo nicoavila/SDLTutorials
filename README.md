73f75268bc914527c8a8f00f04cc5b7f5bbc6944
Nicolas Avila <nicolasavila.a@gmail.com>
2018-12-07 10:42:44 -0300

    feat: commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+# SDLTutorial
+> Tutorial de SDL en C. Muestra una ventana y carga una imagen de un pulpo espacial
+
+## Referencias
+* Game Art: [https://opengameart.org/content/more-assorted-32x32-creatures](https://opengameart.org/content/more-assorted-32x32-creatures)
+* Video: [https://www.youtube.com/watch?v=yFLa3ln16w0](https://www.youtube.com/watch?v=yFLa3ln16w0)
---
+++ SDLTutorials/main.c
@@ -0,0 +1,70 @@
+//
+//  main.c
+//  SDLTutorials
+//
+//  Created by Nicolas Avila on 07-12-18.
+//  Copyright © 2018 Nicolas Avila. All rights reserved.
+//
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2_image/SDL_image.h>
+
+int main(void) {
+    //Inicializa
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        printf("Error al inicializar SDL: %s\n", SDL_GetError());
+    }
+    
+    //Crea la ventana
+    SDL_Window* win = SDL_CreateWindow("Nico SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
+    if (!win) {
+        printf("Error al crear la ventana: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Crea el renderer, que va a configurar el hardware gráfico
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    if (!rend) {
+        printf("Error al crear el renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Carga la imagen en memoria
+    SDL_Surface* surface = IMG_Load("space_squid.png");
+    if (!surface) {
+        printf("Error al crear la superficie: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Carga la rextura
+    SDL_Texture* text = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if (!text) {
+        printf("Error al crear la textura: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Limpia la ventana
+    SDL_RenderClear(rend);
+    
+    //Dibuja la imagen
+    SDL_RenderCopy(rend, text, NULL, NULL);
+    SDL_RenderPresent(rend);
+    
+    SDL_Delay(10000);
+    
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
